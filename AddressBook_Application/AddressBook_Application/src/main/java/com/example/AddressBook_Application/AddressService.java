f8ef6f8c07cc065ef61efd0f3418a8aea0d07014
Aman Prajapati <ap757162@gmail.com>
2025-03-02 12:12:20 +0530

    [Aman] Add. Logging of service layer using Lombook library.

--- AddressBook_Application/AddressBook_Application/src/main/java/com/example/AddressBook_Application/AddressService.java
+++ AddressBook_Application/AddressBook_Application/src/main/java/com/example/AddressBook_Application/AddressService.java
@@ -1,11 +1,13 @@
 package com.example.AddressBook_Application;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class AddressService{
 
@@ -16,7 +18,9 @@ public class AddressService{
     int idCounter = 1;
     // Method to get all the records
     public ResponseEntity<List<Address>> getAllRecords() {
+        log.info("Fetching all the records.");
         if (addressList.isEmpty()) {
+            log.warn("No records found!");
             return ResponseEntity.noContent().build();
         }
             return ResponseEntity.ok(addressList);
@@ -24,15 +28,20 @@ public class AddressService{
 
     // Method to fetch record by ID
     public ResponseEntity<Address> getRecordById(int id){
+        log.info("Fetching the record for the ID {}", id);
           Address address = addressList.stream()
                 .filter(a -> a.getId() == id)
                 .findFirst()
-                .orElseThrow(()-> new RuntimeException("No address found for ID: " + id));
+                .orElseThrow(()-> {
+                    log.error("No record found for ID {}", id);
+                    return new RuntimeException("No address found for ID: " + id);
+                });
         return ResponseEntity.ok(address);
     }
 
     // Method to create a new record
     public ResponseEntity<Address> createRecord(AddressDTO addressDTO){
+        log.info("Creating new record.");
         Address address = new Address();
         address.setId(idCounter++);
         address.setName(addressDTO.getName());
@@ -41,12 +50,14 @@ public class AddressService{
         address.setZipcode(addressDTO.getZipcode());
 
         addressList.add(address);
+        log.debug("Address created successfully: {}", address);
         return ResponseEntity.ok(address);
 
     }
 
     // Method to update an existing record
     public ResponseEntity<Address> updateRecord(int id, AddressDTO addressDTO){
+        log.info("Updating the record for ID {}", id);
         return addressList.stream()
                 .filter(a -> a.getId() == id)
                 .findFirst()
@@ -55,19 +66,28 @@ public class AddressService{
                     a.setCity(addressDTO.getCity());
                     a.setState(addressDTO.getState());
                     a.setZipcode(addressDTO.getZipcode());
+                    log.debug("Record updated for ID {}", id);
                     return ResponseEntity.ok(a);
                 })
 
-                .orElseThrow(()-> new RuntimeException("No address record found for ID: " + id));
+                .orElseThrow(()-> {
+                    log.error("No record found for the ID {}", id);
+                    return new RuntimeException("No address record found for ID: " + id);
+                });
     }
 
     // Method to delete an existing record
     public ResponseEntity<Address> deleteRecord(int id){
+        log.info("Deleting address record for ID: {}", id);
         Address deletedAddress = addressList.stream()
                 .filter(address -> address.getId() == id)
                 .findFirst()
-                .orElseThrow(()-> new RuntimeException("No address record found for ID: " + id));
+                .orElseThrow(()-> {
+                    log.error("No address found for ID {}", id);
+                    return new RuntimeException("No address record found for ID: " + id);
+                  });
             addressList.remove(deletedAddress);
+            log.debug("Deleted address: {}", deletedAddress);
             return ResponseEntity.ok(deletedAddress);
     }
 
